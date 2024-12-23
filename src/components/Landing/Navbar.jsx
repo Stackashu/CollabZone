2cea99201df49c017758dd33e2d86a09ff7401be
Stackashu <ashu12141214@gmail.com>
2024-12-23 15:24:42 +0530

    Navbae responsiveness create

--- src/components/Landing/Navbar.jsx
+++ src/components/Landing/Navbar.jsx
@@ -1,46 +1,94 @@
 import React, { useState } from "react";
 import style from "../../styles/Landing/Navbar.module.css";
-import logo from '../../assests/images/CollabLogo.png'
-import { NavLink  } from "react-router-dom";
+import logo from "../../assests/images/CollabLogo.png";
+import { NavLink } from "react-router-dom";
+import "../../App.css";
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import QuestionMarkOutlinedIcon from '@mui/icons-material/QuestionMarkOutlined';
+import LiveHelpOutlinedIcon from '@mui/icons-material/LiveHelpOutlined';
+import FeaturedPlayListOutlinedIcon from '@mui/icons-material/FeaturedPlayListOutlined';
+import DocumentScannerOutlinedIcon from '@mui/icons-material/DocumentScannerOutlined';
+import GamepadOutlinedIcon from '@mui/icons-material/GamepadOutlined';
 
 const Navbar = () => {
-    const [activeIndex, setActiveIndex] = useState(0);
-    
+  const [isOpen, setIsOpen] = useState(true);
+  const [activeIndex, setActiveIndex] = useState(0);
 
-    const navs = ["Home", "Features", "How It Works", "Testimonials", "FAQs"];
+  const navs = ["Home", "Features", "How It Works", "Testimonials", "FAQs"];
 
   return (
-    <header className={`${style.header}`}>
-
+    <>
+      <header className={`${style.header}`}>
         <div className={`${style.lefthead}`}>
-            <img className={`${style.logo}`} src={logo} alt="CollabZone logo"/>
-            <h1><span>C</span>ollab<span>Z</span>one</h1>
+          <img className={`${style.logo}`} src={logo} alt="CollabZone logo" />
+          <h1>
+            <span>C</span>ollab<span>Z</span>one
+          </h1>
         </div>
 
-      <nav>
-        <ul  className={`${style.navbar} `}>
-        {navs.map((nav, index) => (
-        <li
-          key={index}
-          className={`${style.navbar} ${activeIndex === index ? "changecolor" : ""}`}
-          onClick={() => setActiveIndex(index)} // Set active index on click
-        >
-          {nav}
-        </li>
-      ))}
-        </ul>
+        <nav>
+          <ul className={`${style.navbar} `}>
+            {navs.map((nav, index) => (
+              <li
+                key={index}
+                className={`${style.navbar} ${
+                  activeIndex === index ? "changecolor" : ""}`}
+                onClick={() => setActiveIndex(index)}  >
+                {nav}
+              </li>
+            ))}
+          </ul>
         </nav>
 
-      <div className={`${style.righthead}`}>
-         <NavLink className={`${style.link1}`} to="#">
+        <div className={`${style.righthead}`}>
+          <NavLink className={`${style.link1}`} to="#">
             Get Started for FREE
-         </NavLink>
-         <NavLink className={`${style.link2}`} to="#">
+          </NavLink>
+          <NavLink className={`${style.link2}`} to="#">
             Log in
-         </NavLink>
-      </div>
+          </NavLink>
+        </div>
+      </header>
 
-    </header>
+      {/* mobile and tablet view code  */}
+
+      <header className={`${style.header2}`}>
+      
+        <div className={`${style.lefthead2}`}>
+        <img className={`${style.logo2}`} src={logo} alt="CollabZone logo" />
+          <h1>
+            <span>C</span>ollab<span>Z</span>one
+          </h1>
+        </div>
+        
+       <div  onClick={() => setIsOpen(true)}>
+        <GamepadOutlinedIcon  className={`${style.button}`} /> 
+      </div>
+          
+          {/* navbar pallete  for up and down*/}
+          <nav className={`${style.mainnav} ${isOpen ? style.mainnavup : " "} `}>
+            <ul className={`${style.navbar2}`}>
+              <li><HomeOutlinedIcon style={{ fontSize: 50 }}/> <h4>Home</h4></li>
+              <li><FeaturedPlayListOutlinedIcon style={{ fontSize: 50 }}/><h4>Features</h4></li>
+              <li><QuestionMarkOutlinedIcon style={{ fontSize: 50 }}/><h4>How It Works</h4></li>
+              <li><DocumentScannerOutlinedIcon  style={{ fontSize: 50 }}/><h4>Testimonials</h4></li>
+              <li><LiveHelpOutlinedIcon style={{ fontSize: 50 }}/><h4>FAQs</h4></li>
+            </ul>
+            <div className={`${style.linkbox}`}>
+               <NavLink className={`${style.link1}`} to="#">
+                 Get Started for FREE
+               </NavLink>
+              <NavLink className={`${style.link2}`} to="#">
+                 Log in
+               </NavLink>
+        </div>
+             <CloseOutlinedIcon className={`${style.cross}`} onClick={() => setIsOpen(false)} />
+          
+          </nav>
+      </header>
+
+    </>
   );
 };
 
