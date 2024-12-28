c9ad2ae88720fbcbc41a9fc02ecb707e93c84004
Stackashu <ashu12141214@gmail.com>
2024-12-28 19:47:55 +0530

    how it works create % bug solve

---
+++ src/components/Landing/HowItWorks.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "../../App.css";
+import style from "../../styles/Landing/HowItWorks.module.css";
+import guidevideo from "../../assests/images/guidevideo.jpg";
+import playbutton from "../../assests/images/playbutton.png";
+const HowItWorks = () => {
+  return (
+    <div className={`${style.guide}`}>
+
+      {/* following bgdiv is for the background image of the guide section for prevention for doing low opacity and affect on the other elemnts also*/}
+      <div className={`${style.bgdiv}`}></div>
+
+      <div className={`${style.maindiv}`}>
+
+      <div className={`${style.heading}`}>
+        Your Guide to a Seamless Experience
+      </div>
+      <div className={`${style.video}`}>
+        <div className={`${style.tag}`}>#CollabZone</div>
+
+        <img className={`${style.play}`} src={playbutton} alt={playbutton} />
+
+        <img src={guidevideo} alt="video"></img>
+        <div className={`${style.description}`}>
+          Welcome to CollabZone! This video tutorial is tailored for new users,
+          guiding you through the platform. In just a few minutes, learn how to
+          set up your profile, connect with entrepreneurs, and access key
+          features to enhance your collaboration experience.
+        </div>
+      </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default HowItWorks;
