8181b67b71aec72d7c9b2d9c45bb7570209df2b9
Stackashu <ashu12141214@gmail.com>
2024-12-29 15:11:08 +0530

    Faq's create

--- src/components/Landing/Faq.jsx
+++ src/components/Landing/Faq.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
-
+import Questions from './MiniComponents/Questions'
+import "../../App.css";
+import style from "../../styles/Landing/Faq.module.css";
 const Faq = () => {
+
+  const problems = [
+    {
+      quest: "What is CollabZone?",
+      ans: "CollabZone is a collaborative platform designed to connect entrepreneurs, innovators, and professionals. It provides tools and resources to facilitate partnerships, project management, and effective communication.",
+    },
+    {
+      quest: "Is there a cost to use CollabZone?",
+      ans: "",
+    },
+    {
+      quest: "How can I find partners on CollabZone?",
+      ans: "",
+    },
+
+    {
+      quest: "How does project management work on the platform?",
+      ans: "",
+    },
+
+    {
+      quest: "Is there a cost to use CollabZone?",
+      ans: "",
+    },
+  ];
+  
   return (
-    <div>
-      Faq
+    <div className={`${style.box}`}>
+
+      <div className={`${style.left}`}></div>
+
+      <div className={`${style.right}`}>
+        <h1>Frequently Asked Questions</h1>
+        <div className={`${style.qs}`}>
+          {
+            problems.map((data,idx)=>{
+              return <Questions key={idx} question={data.quest} answer={data.ans}/>
+            })
+          }
+        </div>
+      </div>
+      
     </div>
   )
 }
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -5,7 +5,7 @@ import Hero from "./Hero";
 import Features from "./Features";
 import HowItWorks from "./HowItWorks";
 import Testimonials from "./Testimonials";
-
+import Faq from "./Faq";
 
 const Landing = () => {
   return (
@@ -16,6 +16,7 @@ const Landing = () => {
       <Features />
       <HowItWorks />
      <Testimonials/>
+     <Faq/>
     </div>
   );
 };
